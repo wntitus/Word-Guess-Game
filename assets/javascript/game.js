ae9b428a73c881838417f55dd286cd22b148fc9d
wntitus <wesn.titus@gmail.com>
2018-07-19 11:54:06 -0400

    working on functions and beginning keyup function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,16 @@
+// declaring global wins and losses as well as creating an empty array to hold our underscores
+// also creating alphabet array to make sure later the user is pressing a letter key
+
+
+
+
 var wins = 0;
 var losses = 0;
 var undArray = [];
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+// creating wordBank object that has our words and a method to take the words and fill our underscore array with the 
+// correct amount of underscores
 
 var wordBank = {
     wordOne : ["p", "e", "p", "p", "e", "r", "o", "n", "i"],
@@ -9,15 +18,21 @@ var wordBank = {
     wordThree : ["b", "a", "c", "o", "n"] ,
     wordFour : ["o", "l", "i", "v", "e", "s"] ,
     wordFive : ["m", "u", "s", "h", "r", "o", "o", "m", "s"] ,
-    wordWipe: function(wone) {
-        for (i = 0; i < wone.length; i++) {
-            undArray.push("_");           
-        }
-        return undArray;
-    }
 
 }
 
-var underscores = [wordBank.wordWipe(wordBank.wordOne)];
+wordWipe = function(wone) {
+    for (i = 0; i < wone.length; i++) {
+        undArray.push("_");           
+    }
+    return undArray;
+}
+
+document.onkeyup = function(event) {
+
+    console.log("THE USER PRESSED " + event.key);
+}
+
+var underscores = wordWipe(wordBank.wordOne);
 
 console.log(underscores);
